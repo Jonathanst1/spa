e72170d8607c9d39224aa64d4fcc6f207ed4c732
Jonathanst1 <jonathan418@gmail.com>
2023-01-18 19:57:07 -0300

    modificações no status,tabela, botoões e falta o footer

--- Atendimento/admin.py
+++ Atendimento/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from .models import plano,Acesso
 
+class planoStatus(admin.ModelAdmin):
+    list_display = ['sistema','status']
 
 # Register your models here.
-admin.site.register(plano),
+admin.site.register(plano,planoStatus),
 admin.site.register(Acesso)
+
 
 
 # Register your models here.
